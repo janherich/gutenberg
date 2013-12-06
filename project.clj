c37a4afd7c415cd1c01b51009c7f73a615e51e14
jhe <jan.herich@gmail.com>
2013-12-07 00:11:23 +0100

    Update dependencies
    Updated dependencies to newest versions

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.7.10"]
+                 [org.clojure/tools.reader "0.8.1"]
                  [markdown-clj "0.9.35"]
-                 [enlive "1.1.4"]])
+                 [enlive "1.1.5"]])
